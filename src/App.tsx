daabdc3e3d486bcfebefa8438a2f1676d066316f
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2025-08-24 12:20:58 +0400

    onClick string

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,33 @@ import type {TrackDataItem, TracksResponse} from "./types/types.ts";
 export function App() {
 
     const [tracks, setTracks] = useState<TrackDataItem[]>([])
+    const [selectedTracksIds, setSelectedTracksIds] = useState<string | null>(null)
 
     useEffect(() => {
         fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks', {
-            headers:{
+            headers: {
                 'API-KEY': '9778dd46-146a-43ee-b515-46ccd9995305'
             }
         })
-        .then(res => res.json())
-        .then((json: TracksResponse) => {
-            console.log(json)
-            setTracks(json.data)
-        })
+            .then(res => res.json())
+            .then((json: TracksResponse) => {
+                console.log(json)
+                setTracks(json.data)
+            })
     }, []);
 
     return (
         <>
             <ul>
                 {tracks.map((track) => {
+                    const color = track.id === selectedTracksIds ? 'green' : 'white';
                     return (
                         <li key={track.id}>
-                            <h4>{track.attributes.title}</h4>
+                            <h4 style={{color}}
+                                onClick={ ()=> { setSelectedTracksIds(track.id) } }
+                            >
+                                {track.attributes.title}
+                            </h4>
                             <audio src={track.attributes.attachments[0].url}
                                    controls={true}
                             />
