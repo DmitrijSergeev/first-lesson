eeac38aa41fcbbb20a8078642cd4732c09b4d7c3
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2025-08-24 12:11:01 +0400

    multy array

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,14 @@ import type {TrackDataItem, TracksResponse} from "./types/types.ts";
 export function App() {
 
     const [tracks, setTracks] = useState<TrackDataItem[]>([])
+    const [selectedTrackId, setSelectedTrackId] = useState<string[]>([])
+
+    const toggleTrack = (id: string) => {
+        setSelectedTrackId((prev) =>
+            prev.includes(id) ? prev.filter(
+                (trackId) => trackId !== id) : [...prev, id]
+        )
+    }
 
     useEffect(() => {
         fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks', {
@@ -24,9 +32,15 @@ export function App() {
         <>
             <ul>
                 {tracks.map((track) => {
+                    const isSelected = selectedTrackId.includes(track.id)
+                    const color = isSelected ? 'green' : 'white'
+
                     return (
-                        <li key={track.id}>
-                            <h4>{track.attributes.title}</h4>
+                        <li key={track.id}
+                            style={{color}}
+                        >
+                            <h4 onClick={() => toggleTrack(track.id)}
+                            >{track.attributes.title}</h4>
                             <audio src={track.attributes.attachments[0].url}
                                    controls={true}
                             />
