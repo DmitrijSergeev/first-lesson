9008104780040cac8e96d2d48e079b6f5f5c03b6
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2025-08-26 17:27:12 +0400

    synchrony of two states

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
 import {useEffect, useState} from "react";
-import type {TrackDataItem, TracksResponse} from "./types/types.ts";
-
+import type {TrackDataItem, TrackResponse, TracksResponse} from "./types/types.ts";
 
 export function App() {
 
     const [tracks, setTracks] = useState<TrackDataItem[]>([])
-    const [selectedTracksIds, setSelectedTracksIds] = useState<string | null>(null)
+    const [isLoading, setIsLoading] = useState<boolean>(true)
+    const [selectedTrackId, setSelectedTrackId] = useState<string | null>(null)
+    const [selectedTrack, setSelectedTrack] = useState<TrackResponse | null>(null)
+
+    // const clickedTrack = tracks.find(
+    //     t => t.id === selectedTrack?.id)
 
     useEffect(() => {
         fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks', {
@@ -18,29 +22,60 @@ export function App() {
             .then((json: TracksResponse) => {
                 console.log(json)
                 setTracks(json.data)
+                setIsLoading(false)
             })
     }, []);
 
+    useEffect(() => {
+        if (!selectedTrackId) return
+        fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks' + selectedTrackId, {
+            headers: {
+                'API-KEY': '9778dd46-146a-43ee-b515-46ccd9995305'
+            }
+        })
+            .then(res => res.json())
+            .then((json: TrackResponse) => {
+                setSelectedTrack(json)
+            })
+    }, [selectedTrackId]);
+
     return (
-        <>
-            <ul>
-                {tracks.map((track) => {
-                    const color = track.id === selectedTracksIds ? 'green' : 'white';
-                    return (
-                        <li key={track.id}>
-                            <h4 style={{color}}
-                                onClick={ ()=> { setSelectedTracksIds(track.id) } }
-                            >
-                                {track.attributes.title}
-                            </h4>
-                            <audio src={track.attributes.attachments[0].url}
-                                   controls={true}
-                            />
-                        </li>
-                    )
-                })}
-            </ul>
-        </>
+        <div>
+            <h1>Music Fun</h1>
+            <div style={{display: 'flex', gap: '200px'}}>
+                <ul>
+                    <h2>List</h2>
+                    {
+                        isLoading && <p>Loading....</p>
+                    }
+                    {tracks.map((track) => {
+                        const color = track.id === selectedTrack?.data.id ? 'green' : 'white';
+                        return (
+                            <li key={track.id}>
+                                <h4 style={{color}}
+                                    onClick={() => {
+                                        setSelectedTrackId(track.id)
+                                    }}
+                                >
+                                    {track.attributes.title}
+                                </h4>
+                                <audio src={track.attributes.attachments[0].url}
+                                       controls={true}
+                                />
+                            </li>
+                        )
+                    })}
+                </ul>
+                <div>
+                    <h2>Details</h2>
+                    {selectedTrack && <div>
+                        <div>{selectedTrack.data.attributes.title}</div>
+                        <div>{selectedTrack.data.attributes.addedAt}</div>
+                        <div>likes: {selectedTrack.data.attributes.likesCount}</div>
+                    </div>}
+                </div>
+            </div>
+        </div>
     )
 }
 
